dcbb17c9815360514adba3fe7cb176a858e85e1f
petros santos <satoritmkz@gmail.com>
2025-07-16 17:54:32 +0100

    recipe_sql

--- recipe_app_dump.sql
+++ recipe_app_dump.sql
@@ -1,1 +1,63 @@
-fase 2
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 16-Jul-2025 às 18:51
+-- Versão do servidor: 10.4.32-MariaDB
+-- versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `recipe_app1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `ingrediente`
+--
+
+CREATE TABLE `ingrediente` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `ingrediente`
+--
+
+INSERT INTO `ingrediente` (`id`, `nome`) VALUES
+(1, 'Chocolate'),
+(2, 'Cenoura'),
+(3, 'Farinha'),
+(4, 'Ovos'),
+(5, 'Peito de frango'),
+(6, 'Alface'),
+(7, 'Tomate'),
+(8, 'Croutons'),
+(9, 'Queijo');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `ingrediente`
+--
+ALTER TABLE `ingrediente`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
