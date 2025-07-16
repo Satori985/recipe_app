30192026884557ec63912b335bdf98c94d9508a2
petros santos <satoritmkz@gmail.com>
2025-07-16 18:26:46 +0100

    conexao_a_base_de_dados

--- app.php
+++ app.php
@@ -1,2 +1,13 @@
-fase 2
-continuacao
+<?php
+
+$con = mysqli_connect('localhost', 'root', '', 'recipe');
+
+if ($con) {
+    echo "Conexão com a base de dados concluída!\n";
+} else {
+    echo "Erro na conexão com a base de dados\n";
+    
+}
+
+mysqli_close($con);
+?>
